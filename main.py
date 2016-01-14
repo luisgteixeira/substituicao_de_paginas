9fb6fd261de451f302000826b543f51480aef6cf
Luis Guilherme <luisguilherme.ufpi@gmail.com>
2016-01-14 09:57:01 -0300

    Leitura de arquivo modificada.

--- main.py
+++ main.py
@@ -6,28 +6,17 @@ from fifo import *
 from otm import *
 from lru import *
 
-def main(argv):
-    # Recebe o nome do arquivo de entrada
-    inputfile = sys.argv[-1]
+def main():
+    # Carrega o arquivo em uma lista
+    lines = sys.stdin.readlines()
 
-    if(inputfile == sys.argv[0]): # Verifica se algum arquivo foi recebido
-        print('Arquivo nao pode ser lido!!')
-    else:
-        # Abre o arquivo
-        arq = open(inputfile, 'r')
-        # Carrega o arquivo em uma lista
-        lines = arq.readlines()
+    fifo = FIFO(lines)
+    otm = OTM(lines)
+    lru = LRU(lines)
 
-        fifo = FIFO(lines)
-        otm = OTM(lines)
-        lru = LRU(lines)
-
-        print('FIFO', fifo.output)
-        print('OTM', otm.output)
-        print('LRU', lru.output)
-
-        # Fecha o arquivo
-        arq.close()
+    print('FIFO', fifo.output)
+    print('OTM', otm.output)
+    print('LRU', lru.output)
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    main()
